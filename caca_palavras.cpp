64f336aba3373b5e5691c802bd43b2b46f99591c
Anarco Quaresma Zeferino Nascimento <anarco.qr@gmail.com>
2017-06-11 02:23:39 -0300

    montando caca palavras com, no maximo, 10 palavras na horizontal

--- caca_palavras.cpp
+++ caca_palavras.cpp
@@ -3,34 +3,94 @@
 #include <stdlib.h>
 #include <time.h>
 
+void preenche_matriz_com_letras(char matriz[10][10]){
+	char alfabeto[26] = {'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	//preenche a matriz com as letras do array alfabeto
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			int indice = rand() % 26; //aqui eu gero um valor aleatorio entre 0 e 25. Os indices do array
+			if(matriz[i][j] == 0){
+				matriz[i][j] = alfabeto[indice];	
+			}
+		}
+	}
+}
+
+void imprime_caca_palavras(char matriz[10][10]){
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			printf("%c ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void encaixar_palavra_horizontal(char matriz[10][10], char palavra[]){
+	strupr(palavra);
+	int i = rand() % 10;
+	int j = rand() % 10;
+	int check = 0;
+	
+	while(check == 0){
+		while (j > (10 - strlen(palavra))){
+			j = rand() % 10;
+		}
+		for(int j1 = j; j1 < 10; j1++){
+			if(matriz[i][j1] != 0){
+				check = 2;
+				break;
+			}
+		}
+		if(check == 2){
+			i = rand() % 10; 
+			check = 0;
+		}else{
+			check = 1;
+		}
+	}
+	
+	for(int k = 0;k < strlen(palavra);k++){
+		matriz[i][j] = palavra[k];
+		j++;
+	}
+}
 
 int main(){
 	
-	char alfabeto[26] = {'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	char caca_palavras[10][10];
+	char palavra0[] = "uvinha";
+	char palavra1[] = "bolacha";
+	char palavra2[] = "flor";
+	char palavra3[] = "violao";
+	char palavra4[] = "galinha";
+	char palavra5[] = "foguete";
+	char palavra6[] = "soldado";
+	char palavra7[] = "bomba";
+	char palavra8[] = "carro";
+	char palavra9[] = "gol";
 	
-	srand(time(NULL)); // funcao que prepara a funcao rand() para gerar valores aleatorios
+	char caca_palavras[10][10];
 	
 	//preenche a matriz com zeros, evitando lixos de memoria
 	for(int i = 0; i < 10; i++){
 		memset(caca_palavras[i], 0, 10);
 	}
+	srand(time(NULL)); // funcao que prepara a funcao rand() para gerar valores aleatorios
 	
-	//preenche a matriz com as letras do array alfabeto
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			int indice = rand() % 26; //aqui eu gero um valor aleatorio entre 0 e 25. Os indices do array
-			caca_palavras[i][j] = alfabeto[indice];
-		}
-	}
+	encaixar_palavra_horizontal(caca_palavras, palavra0);
+	encaixar_palavra_horizontal(caca_palavras, palavra1);
+	encaixar_palavra_horizontal(caca_palavras, palavra2);
+	encaixar_palavra_horizontal(caca_palavras, palavra3);
+	encaixar_palavra_horizontal(caca_palavras, palavra4);
+	encaixar_palavra_horizontal(caca_palavras, palavra5);
+	encaixar_palavra_horizontal(caca_palavras, palavra6);
+	encaixar_palavra_horizontal(caca_palavras, palavra7);
+	encaixar_palavra_horizontal(caca_palavras, palavra8);
+	encaixar_palavra_horizontal(caca_palavras, palavra9);
 	
-	//imprime a matriz, porque eu sou printador geral de matriz agora, segundo Raquel.
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			printf("%c ", caca_palavras[i][j]);
-		}
-		printf("\n");
-	}
-
-return 0;
+	preenche_matriz_com_letras(caca_palavras);
+	imprime_caca_palavras(caca_palavras);
+	
+	system("PAUSE");
+	return 0;
 }
