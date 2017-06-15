7ea8dc7dded0d6afff4f613ffc7e99e8296ef7dd
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2017-06-15 11:24:42 -0300

    refatoração

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DEZ 10
-char matriz[DEZ][DEZ];
+#define DEZ 10 //tamanho da matriz do caça palavras
+char matriz[DEZ][DEZ]; //matriz do caça palavras
 
-
+//preenche a matriz com zeros, evitando lixos de memoria
 void inicializa_matriz() {
-    //preenche a matriz com zeros, evitando lixos de memoria
 	for(int i = 0; i < 10; i++){
 		memset(matriz[i], 0, DEZ);
 	}
@@ -39,88 +38,61 @@ void imprime_caca_palavras(){
         }	
 }
 
-void coloca_palavra_diagonal_crescente(char palavra[DEZ]){
-    int tamanho_palavra = strlen(palavra);
-    int linha_primeira_letra_palavra = rand() % (DEZ - tamanho_palavra);
-    int coluna_primeira_letra_palavra = rand() % (DEZ - tamanho_palavra);
+void coloca_palavra_diagonal_decrescente(char palavra[DEZ]){
+    int i = rand() % (DEZ - strlen(palavra)); //linha da primeira letra da palavra
+    int j = rand() % (DEZ - strlen(palavra)); //coluna da primeira letra da palavra
     
-    while (matriz[linha_primeira_letra_palavra][coluna_primeira_letra_palavra] != 0) {
-        linha_primeira_letra_palavra = rand() % (DEZ - tamanho_palavra);
-        coluna_primeira_letra_palavra = rand() % (DEZ - tamanho_palavra);
+    while (matriz[i][j] != 0) {
+        i = rand() % (DEZ - strlen(palavra));
+        j = rand() % (DEZ - strlen(palavra));
     }
-    printf("Inicio da palavra, linha: %d coluna: %d \n",linha_primeira_letra_palavra+1, coluna_primeira_letra_palavra+1);
     
-    for(int i =0; i < tamanho_palavra; i++) {
-        matriz[linha_primeira_letra_palavra][coluna_primeira_letra_palavra] = palavra[i];
-        linha_primeira_letra_palavra++;coluna_primeira_letra_palavra++;
+    for(int k =0; k < strlen(palavra); k++) {
+        matriz[i][j] = palavra[k];
+        i++; j++;
     }
 }
 
-void coloca_palavra_diagonal_decrescente(char palavra[DEZ]){
-    int tamanho_palavra = strlen(palavra);
-    int linha_primeira_letra_palavra = tamanho_palavra + (rand() % (DEZ-tamanho_palavra));
-    int coluna_primeira_letra_palavra = rand() % (DEZ-tamanho_palavra);
+void coloca_palavra_diagonal_crescente(char palavra[DEZ]){
+    int i = strlen(palavra) + (rand() % (DEZ - strlen(palavra))); //linha da primeira letra da palavra
+    int j = rand() % (DEZ - strlen(palavra)); //coluna da primeira letra da palavra
     
-     while (matriz[linha_primeira_letra_palavra][coluna_primeira_letra_palavra] != 0) {
-         linha_primeira_letra_palavra = tamanho_palavra + (rand() % (DEZ-tamanho_palavra));
-         coluna_primeira_letra_palavra = rand() % (DEZ-tamanho_palavra);   
+     while (matriz[i][j] != 0) {
+         i = strlen(palavra) + (rand() % (DEZ - strlen(palavra)));
+         j = rand() % (DEZ - strlen(palavra));   
      }
     
-    printf("Inicio da palavra, linha: %d coluna: %d \n",linha_primeira_letra_palavra+1, coluna_primeira_letra_palavra+1);
-    
-    for(int i =0; i < tamanho_palavra; i++) {
-        matriz[linha_primeira_letra_palavra][coluna_primeira_letra_palavra] = palavra[i];
-        linha_primeira_letra_palavra--;coluna_primeira_letra_palavra++;
+    for(int k =0; k < strlen(palavra); k++) {
+        matriz[i][j] = palavra[k];
+        i--; j++;
     }
 }
 
-char* inverter(char palavra[10]){
-     char *array_dinamico = malloc(10*sizeof(char));
+char* inverte_palavra(char palavra[10]){
+     char *palavra_invertida = malloc(10*sizeof(char));
      int i;
      int f = 0;
      for (i = (strlen(palavra)-1); i>=0; i--){
-              array_dinamico[f] = palavra[i];  
+              palavra_invertida[f] = palavra[i];  
               f++;
          } 
-     return array_dinamico;
+     return palavra_invertida;
 }
 
 int main() {
     
     srand(time(NULL));
     inicializa_matriz();
+    
     char palavra1[10] = "raquel";
-    char palavra2[10] = "ambrozio";
-    char palavra3[10] = "da";
-    char palavra4[10] = "fonseca";
-    coloca_palavra_diagonal_crescente(palavra1);
-    coloca_palavra_diagonal_crescente(palavra2);
-    coloca_palavra_diagonal_crescente(palavra3);
-    coloca_palavra_diagonal_crescente(palavra4);
+    char *palavra1_invertida = inverte_palavra(palavra1);
+
+    coloca_palavra_diagonal_decrescente(palavra1);
+    coloca_palavra_diagonal_crescente(palavra1_invertida);
     preenche_matriz_com_letras();
-    imprime_caca_palavras();
-    
-    /*char palavraa[10] = "raquel";
-    char *oi = inverter(palavraa);
-    printf("%s",oi);
-    
-
-    preenche_matriz();
-    imprime_caca_palavras();
-    printf("\n");*/
-    
-    
-    
-    /*coloca_palavra_diagonal_decrescente(palavra);
-    printf("\n");
-    imprime_caca_palavras();
-
-    
-    /*char letra = gera_letra_aleatoria();
-    char letras[TAMANHOMATRIZ][TAMANHOMATRIZ];
-    letras[1][1] = letra;
-    printf("%c \n", letras[1][1]);*/
-
+    imprime_caca_palavras(); 
+   
     return 0;
 }
+
 
